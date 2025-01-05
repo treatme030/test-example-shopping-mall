fedc536b190b5997d69c64ac56be4c00dd959e90
treatme030 <treatme8192@gmail.com>
2025-01-05 15:53:42 +0900

    feat: RTL 비동기 유틸 할수를 통한 노출 테스트 작성

--- src/pages/home/components/tests/ProductList.spec.jsx
+++ src/pages/home/components/tests/ProductList.spec.jsx
@@ -27,11 +27,57 @@ vi.mock('react-router-dom', async () => {
   };
 });
 
-it('로딩이 완료된 경우 상품 리스트가 제대로 모두 노출된다', async () => {});
+it('로딩이 완료된 경우 상품 리스트가 제대로 모두 노출된다', async () => {
+  await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
 
-it('보여줄 상품 리스트가 더 있는 경우 show more 버튼이 노출되며, 버튼을 누르면 상품 리스트를 더 가져온다.', async () => {});
+  /**
+   * findBy~
+   * 비동기적으로 동작하는 코드로 인한 요소를 쿼리 할 때 사용
+   * 쿼리가 통과하거나 시간 초과될 때까지 재시도
+   * waitFor를 사용해도 작성 가능, findBy~쿼리 역시 내부적으로 waitFor를 사용
+   */
+  const productCards = await screen.findAllByTestId('product-card');
+  expect(productCards).toHaveLength(PRODUCT_PAGE_LIMIT);
 
-it('보여줄 상품 리스트가 없는 경우 show more 버튼이 노출되지 않는다.', async () => {});
+  productCards.forEach((el, index) => {
+    const productCard = within(el);
+    const product = data.products[index];
+
+    expect(productCard.getByText(product.title)).toBeInTheDocument();
+    expect(productCard.getByText(product.category.name)).toBeInTheDocument();
+    expect(
+      productCard.getByText(formatPrice(product.price)),
+    ).toBeInTheDocument();
+    expect(
+      productCard.getByRole('button', { name: '장바구니' }),
+    ).toBeInTheDocument();
+    expect(
+      productCard.getByRole('button', { name: '구매' }),
+    ).toBeInTheDocument();
+  });
+});
+
+it('보여줄 상품 리스트가 더 있는 경우 show more 버튼이 노출되며, 버튼을 누르면 상품 리스트를 더 가져온다.', async () => {
+  const { user } = await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+
+  // show more 버튼의 노출 여부를 판단하기 위해 findBy 쿼리를 사용하여 먼저 첫 페이지에 해당하는 상품 목록이 렌더링되는 것을 기다려야 함
+  await screen.findAllByTestId('product-card');
+
+  expect(screen.getByRole('button', { name: 'Show more' })).toBeInTheDocument();
+
+  const moreBtn = screen.getByRole('button', { name: 'Show more' });
+  await user.click(moreBtn);
+
+  const productCards = await screen.findAllByTestId('product-card');
+  expect(productCards).toHaveLength(PRODUCT_PAGE_LIMIT * 2);
+});
+
+it('보여줄 상품 리스트가 없는 경우 show more 버튼이 노출되지 않는다.', async () => {
+  // 모킹 데이터보다 많은 수로 limit 설정
+  await render(<ProductList limit={50} />);
+
+  expect(screen.queryByText('show more')).not.toBeInTheDocument();
+});
 
 describe('로그인 상태일 경우', () => {
   beforeEach(() => {
